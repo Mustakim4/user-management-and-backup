b346754cc4eaa9a0f564b252a4087c0589d0298e
Mustakim4 <mustakquimdiwan@gmail.com>
2024-09-12 02:49:20 +0000

    change in 2 files

--- create_user.sh
+++ create_user.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "######################################"
 read -p "give the name of user to create " CREATE_USER
-sudo useradd $CREATE_USER
+sudo adduser $CREATE_USER
 echo "$CREATE_USER has created"
 echo "give the password for $CREATE_USER"
 sudo passwd $CREATE_USER
--- delete_user.sh
+++ delete_user.sh
@@ -2,7 +2,7 @@
 echo "######################################"
 read -p "give the name of user to delete " DELETE_USER
 read -p "do you want to delete the home directory of user? " ANSWER_YN
-if [ $ANSWER_YN == yes ];
+if [ $ANSWER_YN == y ];
 then
     sudo userdel -r $DELETE_USER 
 else    
